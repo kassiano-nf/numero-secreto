71e2bccfbcd2e1f9617b535b774f187d07051e1a
kassiano-nf <kassianodaasilvarodrigues@gmail.com>
2024-03-05 22:55:57 +0100

    alteramos de 10 para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+gitlet listaDeNumerosSorteados = [];
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
